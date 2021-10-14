e84986f0e1af3d5d3f426e42a21c3c47a04a4671
Mohammad Nur Rizki <mnurrizki25@gmail.com>
2021-10-15 02:24:03 +0700

    Add additial JWT repo

--- update.php
+++ update.php
@@ -1,5 +1,7 @@
 <?php 
-if ($_SERVER['SERVER_NAME']=='localhost'){
+$server=array();
+$server = ['localhost','farewell.gabut.ga'];
+if ( in_array($_SERVER['SERVER_NAME'],$server)){
 shell_exec( 'git pull origin master' ); ?>
 <h2>Succesfully Pulled</h2>
 <?php     
