e5316f9eb730defd33af0bdb7c09f23801023376
Mohammad Nur Rizki <mnurrizki25@gmail.com>
2021-10-19 00:29:48 +0700

    Fixing Timer

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ function secondsToTime($seconds) {
 }
 $timezone = new DateTimeZone('Asia/Jakarta');
 $date = new DateTime('now',$timezone);
-$date2 = new DateTime('2021-10-22 17:00:00');
+$date2 = new DateTime('2021-10-23 00:00:00',$timezone);
 $diff = $date2->getTimestamp() - $date->getTimestamp();
 $hasil = explode(",",gmdate("j,H,i,s"),$diff);
 
