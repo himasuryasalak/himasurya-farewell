99782435311289c3949b832dc25e844e1269ebb8
Mohammad Nur Rizki <mnurrizki25@gmail.com>
2021-10-18 22:16:44 +0700

    Update popup

--- index.php
+++ index.php
@@ -370,10 +370,10 @@ function downloadEticket(){
             swal.fire({
                 icon: 'success',
                 title:"Oke, sudah...",
-                html:`Kamu berhasil terdaftar, <b>E-Ticket</b> otomatis akan terunduh sesaat lagi. <b>Tunjukkan E-Ticket tersebut saat masuk ke venue</b>, jadi jangan dihapus ya mas mbak. 
+                html:`<b>E-Ticket</b> otomatis akan terunduh sesaat lagi. <b>Tunjukkan E-Ticket tersebut saat masuk ke venue</b>, jadi jangan dihapus ya mas mbak. 
                 <br><br>
                 <button type="button" onclick='javascript:window.location.reload()' class="btn btn-primary"><b>Oke</b></button>`,
-                footer: "<a href='' style='text-align: center;' onclick='javascript:downloadEticket()'>Klik disini jika E-Ticket belum terunduh / terdownload</a>",
+                footer: "<a href='#' style='text-align: center;' onclick='javascript:downloadEticket()'>Klik disini jika E-Ticket belum terunduh / terdownload</a>",
                 showConfirmButton: false,
                 allowOutsideClick:false,
             })
